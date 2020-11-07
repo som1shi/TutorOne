f7bd57be341fea09e82e3f7c151b7115f0f9d765
Sarvagya Somvanshi <sarvagyasomvanshi@Sarvagyas-MacBook-Air.local>
2020-11-07 21:41:44 +0100

    added firebase

--- lib/letters.dart
+++ lib/letters.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
-void main() => runApp(new Photography());
+void main() => runApp(Photography());
 
+/**final File imageFile = getImageFile();
+final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(imageFile);
+final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
+ */
 class Photography extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
