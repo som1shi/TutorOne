2a012421a7446782b8e3fcc4d89b427573cbcca8
Sarvagya Somvanshi <sarvagyasomvanshi@Sarvagyas-MacBook-Air.local>
2020-11-07 18:43:02 +0100

    !!

---
+++ lib/letters.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new Photography());
+
+class Photography extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/text.dart
+++ lib/text.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:TutorOne/letters.dart';
 
 void main() => runApp(new Lobby());
 
 class Lobby extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+    return Container(
+      padding: const EdgeInsets.all(32),
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/background.png"), fit: BoxFit.cover)),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.pink[100],
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: const EdgeInsets.symmetric(vertical: 210.0),
+                ),
+                const Image(
+                  image: AssetImage('assets/A.gif'),
+                ),
+                RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text("Begin!"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => new Photography()),
+                      );
+                    },
+                    textColor: Colors.lightBlue),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
