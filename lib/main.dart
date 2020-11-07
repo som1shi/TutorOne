6b793499e868b30a05d7a33b40ce55612d6bf58b
Sarvagya Somvanshi <sarvagyasomvanshi@Sarvagyas-MacBook-Air.local>
2020-11-07 11:13:54 +0100

    added Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:lib/home.dart';
+import 'package:TutorOne/text.dart';
 
-void main() => runApp(new TutorApp());
+void main() => runApp(MaterialApp(home: TutorApp()));
 
 class TutorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/background.png"), fit: BoxFit.cover)),
-        padding: const EdgeInsets.all(32),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 210.0),
-                    child: Text(
-                      'TutorApp',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.none,
-                          color: Colors.lightBlue),
-                    ),
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/background.png"), fit: BoxFit.cover)),
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 210.0),
+                  child: Text(
+                    'TutorApp',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.none,
+                        color: Colors.lightBlue),
                   ),
-                  Container(
-                      child: RaisedButton(
+                ),
+                RaisedButton(
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    onPressed: () {},
+                        borderRadius: BorderRadius.circular(20)),
                     child: Text("Get Started"),
-                  ))
-                ],
-              ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => new Lobby()),
+                      );
+                    },
+                    textColor: Colors.lightBlue),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/text.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new Lobby());
+
+class Lobby extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
